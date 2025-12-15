d466924d429d851fbc6c4b6e88074a7b195f9171
dejworj <dejworj@shure.com>
2025-12-15 10:11:29 -0600

    basic repl app, started exit command

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,25 @@
+import { createInterface } from 'node:readline';
+import { stdin, stdout } from 'node:process';
+import commandExit from "./command_exit.js"
+
+export function startREPL() {
+    const rl = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex >",
+    });
+    rl.prompt();
+    rl.on("line", async (line) => {
+        const words = cleanInput(line);
+        if(words.length === 0) {
+            rl.prompt();
+            return;
+        }
+        
+        rl.prompt();
+    });
+}
+
 export function cleanInput(input: string): string[] {
     return input
     .toLocaleLowerCase()
